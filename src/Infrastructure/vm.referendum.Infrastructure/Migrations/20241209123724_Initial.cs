78d49cbf8ccbf5180eb5dae5e178efa7f4a773b8
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2025-01-11 11:51:21 +0400

    commit

---
+++ src/Infrastructure/vm.referendum.Infrastructure/Migrations/20241209123724_Initial.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace vm.referendum.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "outbox_message",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    event_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    state = table.Column<int>(type: "integer", nullable: false),
+                    modified_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    content = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_outbox_message", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "permissions",
+                columns: table => new
+                {
+                    value = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Id", x => x.value);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    value = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_roles", x => x.value);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    role_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_roles", x => new { x.role_id, x.user_id });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role_permission",
+                columns: table => new
+                {
+                    role_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    permission_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_role_permission", x => new { x.role_id, x.permission_id });
+                    table.ForeignKey(
+                        name: "fk_role_permission_permissions_permission_id",
+                        column: x => x.permission_id,
+                        principalTable: "permissions",
+                        principalColumn: "value",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_role_permission_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "value",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    role_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    role_value = table.Column<Guid>(type: "uuid", nullable: true),
+                    password_hash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_users_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "value",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_users_roles_role_value",
+                        column: x => x.role_value,
+                        principalTable: "roles",
+                        principalColumn: "value");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "answers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    is_selected = table.Column<bool>(type: "boolean", nullable: false),
+                    question_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    percentage = table.Column<int>(type: "integer", nullable: true),
+                    statistic_value = table.Column<int>(type: "integer", nullable: true),
+                    text = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_answers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "questions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    text_content = table.Column<string>(type: "text", nullable: false),
+                    category_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    answer_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_questions", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_questions_answers_answer_id",
+                        column: x => x.answer_id,
+                        principalTable: "answers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_questions_categories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "permissions",
+                columns: new[] { "value", "name" },
+                values: new object[,]
+                {
+                    { new Guid("80a0d8c0-8a64-426a-9331-71c4bbbe0547"), "ReadMember" },
+                    { new Guid("8f22e919-4c82-4bdc-a04a-b29a901e1f1a"), "WriteMember" },
+                    { new Guid("bd7f8542-13b7-4e20-8651-d8ea4f25d8a3"), "UpdateMember" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "value", "name" },
+                values: new object[,]
+                {
+                    { new Guid("307535ba-70b0-4351-b86d-153176d22de7"), "Admin" },
+                    { new Guid("70195640-f90b-4547-8f73-ee2e2689a5af"), "SuperAdmin" },
+                    { new Guid("e2abc25a-d7c9-4d71-a8d6-9b6a5008d2ed"), "Basic" },
+                    { new Guid("fc8d42c4-44b7-4b68-a41f-221e71a40fff"), "Moderator" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_answers_question_id",
+                table: "answers",
+                column: "question_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_questions_answer_id",
+                table: "questions",
+                column: "answer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_questions_category_id",
+                table: "questions",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_role_permission_permission_id",
+                table: "role_permission",
+                column: "permission_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_role_id",
+                table: "users",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_role_value",
+                table: "users",
+                column: "role_value");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_answers_questions_question_id",
+                table: "answers",
+                column: "question_id",
+                principalTable: "questions",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_answers_questions_question_id",
+                table: "answers");
+
+            migrationBuilder.DropTable(
+                name: "outbox_message");
+
+            migrationBuilder.DropTable(
+                name: "role_permission");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "permissions");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "questions");
+
+            migrationBuilder.DropTable(
+                name: "answers");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
