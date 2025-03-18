ec8cf5b841bbe45de5b7c06bc6c232998e86c0e6
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2025-03-18 13:11:20 +0400

    fix namespaces

--- src/vm.referendum.Domain/Exception/FirstName/FirstNameException.cs
+++ src/vm.referendum.Domain/Exception/FirstName/FirstNameException.cs
@@ -1,6 +1,6 @@
 using Framework.Abstractions.Exceptions;
 
-namespace vm.referendum.Domain.Exception.FirstNameErrors;
+namespace vm.referendum.Domain.Exception.FirstName;
 
 public class FirstNameException(string code, string message)
     : InflowException(code, message);
